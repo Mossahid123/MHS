2cb55feb3482e22251c4647d153f7ff6620f487a
Mossahid123 <mossahidhossain309@gmail.comm>
2022-08-08 02:29:13 +0600

    parts component has changed

--- src/Pages/Home/Parts.js
+++ src/Pages/Home/Parts.js
@@ -10,9 +10,9 @@ const Parts = () => {
     }, [])
     return (
         <div className='mx-10 mt-10'>
-            <h1 className="text-5xl text-center mb-10 font-bold drop-shadow-lg">Welcom To <span className='text-blue-500'> Our World</span> </h1>
+            <h1 className="text-5xl text-center mb-10 font-bold drop-shadow-lg">𝚆𝚎𝚕𝚌𝚘𝚖𝚎 𝚃𝚘 <span className='text-blue-500'>𝙾𝚞𝚛 𝚆𝚘𝚛𝚕𝚍</span> </h1>
             <h2 className='text-2xl font-extrabold drop-shadow-lg mb-2'>NEW <span className='text-blue-500'>ARRIVALS</span></h2>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2'>
                 {
                     parts.slice(0, 6).map(part => <PartsCard
                         key={part._id}
--- src/Pages/Home/PartsCard.js
+++ src/Pages/Home/PartsCard.js
@@ -9,21 +9,18 @@ const PartsCard = ({ part }) => {
     }
     const { _id, img, price, order, quantity, description, name } = part;
     return (
-        <div className="card bg-base-100 shadow-xl">
-            <figure className="px-10 pt-10">
-                <img src={img} alt="Shoes" className="rounded-xl" />
-            </figure>
-            <div className="card-body items-center text-center">
-                <h2 className="card-title">{name}</h2>
-                <p>Price : {price}</p>
-                <p>Minimum Order : {order}</p>
-                <p>Available Quantity : {quantity}</p>
-                <p>Description : {description.slice(0, 60)}</p>
-                <div className="card-actions">
-                    <button onClick={()=>handleNavigate(_id)} className="btn">Buy Now</button>
+        <div class="flex flex-col items-center justify-center max-w-sm mx-auto mt-10">
+            <div class="w-full h-64 bg-gray-300 bg-center bg-cover rounded-lg shadow-md lg:w-80 " style={{ backgroundImage: `url(${img})` }}></div>
+
+            <div class="w-56 -mt-10 overflow-hidden bg-blue-400 rounded-lg shadow-lg md:w-64 lg:w-64">
+                <h3 class="py-2 font-bold tracking-wide text-center text-white uppercase">{name}</h3>
+                <div class="flex items-center justify-between px-3 py-2 bg-gray-200 dark:bg-gray-700">
+                    <span class="font-bold text-gray-800 dark:text-gray-200">${price}</span>
+                    <button className="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-200 transform font-bold bg-blue-400 rounded hover:bg-blue-700 focus:bg-gray-700  focus:outline-none" onClick={handleNavigate} >Details</button>
                 </div>
             </div>
         </div>
+
     );
 };
 
